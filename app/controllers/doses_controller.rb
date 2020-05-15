4f3871e96d937628cc8da83ab5666f3ab9af1bc3
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-05-15 11:22:26 +0200

    push before heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,11 +8,10 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.ingredient = Ingredient.find(params[:dose][:ingredient]) unless params[:dose][:ingredient].blank?
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render cocktail_path(@cocktail)
     end
   end
 
@@ -30,7 +29,7 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
   def ingredient_params
